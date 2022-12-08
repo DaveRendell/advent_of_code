879b70dd4032152916aebd033d063f97e342ee18
David Rendell <David.Rendell@Softwire.com>
2022-12-08 22:06:33 +0000

    Starting 2021 Day 16

---
+++ 2021/16_packet_decoder/main.ts
@@ -0,0 +1,13 @@
+import readLines from "../../utils/readLines"
+import inputFile from "../../utils/inputFile"
+
+function partOne() {
+  const input = readLines(__dirname, inputFile())
+
+  console.log("(P1) Answer: ")
+}
+
+function partTwo() {}
+
+partOne()
+partTwo()
