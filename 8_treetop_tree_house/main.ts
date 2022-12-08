d1f92b206930d9101ffd5e4121b9e2a20392e579
David Rendell <David.Rendell@Softwire.com>
2022-12-08 10:08:40 +0000

    Day 8 part 2

--- 8_treetop_tree_house/main.ts
+++ 8_treetop_tree_house/main.ts
@@ -1,58 +1,74 @@
 import readLines from "../utils/readLines"
 import inputFile from "../utils/inputFile"
 import { range } from "../utils/numbers"
-import { sum } from "../utils/reducers"
+import { max, sum } from "../utils/reducers"
 
-interface HeightCheckState {
-  isVisible: boolean[]
-  currentMax: number
+interface VisibilityAccumulator {
+  visible: boolean[]
+  max: number
 }
 
-const checkRow = ({ isVisible, currentMax }: HeightCheckState, next: number, index: number): HeightCheckState => ({
-  isVisible: isVisible.map((value, i) => i === index ? value || next > currentMax : value),
-  currentMax: Math.max(currentMax, next)
+const visiblityFromLeft = ({ visible, max }: VisibilityAccumulator, next: number): VisibilityAccumulator => ({
+  visible: [...visible, next > max],
+  max: Math.max(max, next)
 })
+
+const getVisibilityFromLeft = (grid: number[][]) => grid.map(row =>
+  row.reduce(visiblityFromLeft, { visible: [], max: -1 }).visible)
 
 function partOne() {
   const treeGrid = readLines(__dirname, inputFile())
     .map(line => [...line].map(char => parseInt(char)))
 
-  const initialState = { isVisible: Array(treeGrid[0].length).fill(false), currentMax: -1 }
+  const fromLeft = getVisibilityFromLeft(treeGrid)
+  const fromRight = flipH(getVisibilityFromLeft(flipH(treeGrid)))
+  const fromTop = transpose(getVisibilityFromLeft(transpose(treeGrid)))
+  const fromBottom = transpose(flipH(getVisibilityFromLeft(flipH(transpose(treeGrid)))))
 
-  const fromLeft = treeGrid.map(row =>
-    row.reduce(checkRow, initialState).isVisible)
-  const fromRows = treeGrid.map((row, rowIndex) =>
-    row.slice()
-      .reverse()
-      .reduce(
-        checkRow,
-        { isVisible: fromLeft[rowIndex].reverse(), currentMax: -1 }).isVisible.reverse())
-  const fromTopAlso = transpose(transpose(treeGrid).map((row, rowIndex) =>
-    row.reduce(checkRow, { isVisible: transpose(fromRows)[rowIndex], currentMax: -1 }).isVisible))
-  const isVisible = transpose(transpose(treeGrid).map((row, rowIndex) =>
-    row.slice()
-      .reverse()
-      .reduce(checkRow, { isVisible: transpose(fromTopAlso)[rowIndex].reverse(), currentMax: -1 }).isVisible.reverse()))
-  
-  const answer = isVisible.map(row => row.filter(value => value).length).reduce(sum)
+  const visibility = [fromBottom, fromTop, fromRight].reduce(mergeBoolGrid, fromLeft)
 
+  const answer = visibility.map(row => row.filter(v => v).length).reduce(sum)
   console.log("(P1) Answer: " + answer)
 }
 
-function partTwo() {}
+const getScoreFromLeft = (grid: number[][]) =>
+  grid.map(row => row.map((cell, i) =>
+    i - (row.slice(0, i)
+      .map((tree, j) => [j, tree])
+      .filter(([j, tree]) => tree >= cell)
+      .pop() || [0])[0]))
 
-partOne()
-partTwo()
+function partTwo() {
+  const treeGrid = readLines(__dirname, inputFile())
+    .map(line => [...line].map(char => parseInt(char)))
+
+  const fromLeft = getScoreFromLeft(treeGrid)
+  const fromRight = flipH(getScoreFromLeft(flipH(treeGrid)))
+  const fromTop = transpose(getScoreFromLeft(transpose(treeGrid)))
+  const fromBottom = transpose(flipH(getScoreFromLeft(flipH(transpose(treeGrid)))))
+
+  const scores = [fromBottom, fromTop, fromRight].reduce(mergeScoreGrid, fromLeft)
+
+  const answer = scores.map(row => row.reduce(max)).reduce(max)
+  console.log("(P2) Answer: " + answer)
+}
+
 
 function transpose<T>(grid: T[][]) {
   return range(0, grid[0].length).map(colIndex => grid.map(row => row[colIndex]))
 }
 
-function printGrid(grid: number[][], results: boolean[][]) {
-  console.log(
-    grid
-      .map((line, rowIndex) => line
-        .map((value, colIndex) => results[rowIndex][colIndex] ? `\x1b[32m${value}\x1b[0m` : value.toString())
-        .join(" "))
-      .join("\n"))
+function flipH<T>(grid: T[][]) {
+  return grid.map(row => row.slice().reverse())
 }
+
+function mergeBoolGrid(grid1: boolean[][], grid2: boolean[][]): boolean[][] {
+  return grid1.map((row, rowIndex) => row.map((cell, colIndex) => cell || grid2[rowIndex][colIndex]))
+}
+
+function mergeScoreGrid(grid1: number[][], grid2: number[][]): number[][] {
+  return grid1.map((row, rowIndex) => row.map((cell, colIndex) => cell * grid2[rowIndex][colIndex]))
+}
+
+partOne()
+partTwo()
