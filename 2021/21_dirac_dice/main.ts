0d0c73a9f8b5cf9382b9df6be1b670227aba4512
David Rendell <David.Rendell@Softwire.com>
2022-12-13 13:51:34 +0000

    2021 Day 21 Part 2

--- 2021/21_dirac_dice/main.ts
+++ 2021/21_dirac_dice/main.ts
@@ -63,34 +63,20 @@ type UniverseCount = number[][][][]
 function partTwo() {
   const playerStartPositions = readLines(__dirname, inputFile())
     .map(line => parseInt(line.at(-1)))
-  
-  const initialState: StateP2 = {
-    player: 0,
-    positions: playerStartPositions,
-    scores: [0, 0],
-    universes: 1
-  }
 
   let universes: UniverseCount = range(0, 21).map(() => range(0, 21).map(() => range(0, 10).map(() => range(0, 10).map(() => 0))))
-  universes[0][0][playerStartPositions[0]][playerStartPositions[1]] = 1
+  universes[0][0][playerStartPositions[0] - 1][playerStartPositions[1] - 1] = 1
   let player = 0
   let stillPlaying = false
   let wins = [0, 0]
-  let winsByTurn = []
-  let universesByTurn = []
-  let turn = 1
   do {
-    console.log("Turn " + turn++)
     stillPlaying = false
     let universesStillInPlay = range(0, 21).map(() => range(0, 21).map(() => range(0, 10).map(() => range(0, 10).map(() => 0))))
-    let turnWins = 0
-    let turnUniverses = 0
     universes.forEach((a, p1Score) =>
       a.forEach((b, p2Score) =>
         b.forEach((c, p1Position) =>
           c.forEach((universeCount, p2Position) => {
             if (universeCount > 0) {
-              turnUniverses += universeCount
               Object.keys(rollCombos).forEach(rollResult => {
                 const newUniverses = rollCombos[rollResult]
                 const position = (player === 0 ? p1Position : p2Position) + 1 // 1 to 10
@@ -99,47 +85,21 @@ function partTwo() {
                 const newScore = score + newPosition
                 if (newScore >= 21) {
                   wins[player] += universeCount * newUniverses
-                  turnWins += universeCount * newUniverses
                 } else {
                   stillPlaying = true
                   const newP1Position = player === 0 ? (newPosition - 1) : p1Position
                   const newP2Position = player === 0 ? p2Position : (newPosition - 1)
                   const newP1Score = player === 0 ? newScore : p1Score
                   const newP2Score = player === 0 ? p2Score : newScore
-                  universesStillInPlay[newP1Score][newP2Score][newP1Position][newP2Position] = universeCount * newUniverses
+                  universesStillInPlay[newP1Score][newP2Score][newP1Position][newP2Position] += universeCount * newUniverses
                 }
               })
             }            
           }))))
     player = (player + 1) % 2
     universes = universesStillInPlay
-    winsByTurn.push(turnWins)
-    universesByTurn.push(turnUniverses)
   } while (stillPlaying)
   console.log("(P1) Answer: " + wins.reduce(max))
-  console.log(winsByTurn)
-  console.log(universesByTurn)
-}
-
-interface StateP2 {
-  universes: number
-  player: number
-  positions: number[]
-  scores: number[]
-}
-
-const processP2 = ({ player, positions, scores, universes }: StateP2): StateP2[] => {
-  return Object.keys(rollCombos).map(rollResult => {
-    const newUniverses = rollCombos[rollResult]
-    const newPosition = wrapPosition(positions[player] + parseInt(rollResult))
-    const newScore = scores[player] + newPosition
-    return {
-      player: (player + 1) % 2,
-      positions: positions.map((p, i) => i === player ? newPosition : p),
-      scores: scores.map((s, i) => i === player ? newScore : s),
-      universes: universes * newUniverses
-    }
-  })
 }
 
 partOne()
