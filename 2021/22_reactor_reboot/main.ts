9f19e147e7130d9fb575175871014850fe4227cf
David Rendell <David.Rendell@Softwire.com>
2022-12-13 16:33:07 +0000

    2021 Day 22 part 2 notes

--- 2021/22_reactor_reboot/main.ts
+++ 2021/22_reactor_reboot/main.ts
@@ -21,7 +21,74 @@ function partOne() {
   console.log("(P1) Answer: " + onCubes)
 }
 
-function partTwo() {}
+function partTwo() {
+  /*
+  OK so how to do...
+  Think about 2d to make it easier to reason
+
+  -x--
+  -x--
+  -x--
+  ----
+
+  ----
+  xxx-
+  xxx-
+  ----
+
+  ----
+  ----
+  -o--
+  ----
+
+  want =>
+
+  oxoo
+  xxxo
+  xoxo
+  oooo
+
+  (1, 1)x(0, 2) =>
+    (1, 1)x(0, 2) [5]
+    [5]
+  (0, 2)x(1, 2) =>
+    (1, 1)x(0, 0) [1]
+    (0, 2)x(1, 2) [6]
+    [7]
+  (1, 1)o(2, 2) =>
+    (1, 1)x(1, 2) ->
+    (1, 1)x(0, 0) [1]
+    (0, 0)x(1, 2) [2]
+    (1, 1)x(1, 1) [1]
+    (2, 2)x(1, 2) [2]
+    [7]
+    Figure out nice algorithms for splitting up cubes
+    One for adding, one for removing
+
+    (x0_min, x0_max) x (y0_min, y0_max)
+    (x1_min, x1_max) x (y1_min, y1_max)
+    if old fully contains new:
+      < filter out this case first>
+    if new fully contains old:
+      new
+    if overlaps(x0, x1) and overlaps(y0, y1):
+      (x0_min, x1_min - 1) x (y0_min, y0_max)
+      (max(x0_min, x1_min), min(x0_max, x1_max))     x (y0_min, y1_min - 1)
+      (max(x0_min, x1_min), min(x0_max, x1_max))     x (y1_max + 1, y0max)
+      (x1_max+1, x0max)    x (y0_min, y0_max)
+      filter out non existant rectangles
+    else
+      (x1_min, x1_max) x (y1_min, y1_max)
+    
+    add new to end
+    Oh! Neat part about this is for `off` commands you can do the exact same
+    algorithm, just don't add `new` at the end!
+
+    make a helper function for measuring cuboids that deals with reverse ranges and
+    measures them as 0
+
+  */
+}
 
 interface Instruction {
   state: boolean,
