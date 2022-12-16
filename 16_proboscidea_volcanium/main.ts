1a70ecbcc4f51d7f2953eb0b6de779ae274bcadc
David Rendell <David.Rendell@Softwire.com>
2022-12-16 11:33:43 +0000

    Fast path trimming

--- 16_proboscidea_volcanium/main.ts
+++ 16_proboscidea_volcanium/main.ts
@@ -7,28 +7,13 @@ function partOne() {
   const valves = readLines(__dirname, inputFile()).reduce(parseValve, {})
 
   const distances = Object.fromEntries(
-    Object.keys(valves).map(name => [name, movementCosts(valves)(name)])
+    Object.keys(valves)
+      .filter(name => valves[name].flowRate > 0 || name === "AA")
+      .map(name => [name, movementCosts(valves)(name)])
   )
-  const paths = getPaths(distances)("AA", 30, [])
-
-  // const initialState: State = { position: "AA", openValves: {}, }
-
-  // let states: State[] = [initialState]
-  // for (let minute = 1; minute <= 30; minute++) {
-  //   console.log("Minute " + minute)
-  //   const possibleStates = states.flatMap(state => getMoves(valves)(state, minute))
-  //   const equivalentStates = possibleStates.reduce(groupBy(stateCharacteristic), {})
-  //   const sorter = pressureReleased(valves, minute)
-  //   const byPressureReleasedDesc = (a, b) => descending(sorter(a), sorter(b))
-  //   states = Object.values(equivalentStates)
-  //     .map(group => group.sort(byPressureReleasedDesc)[0])
-  // }
-
-  // const mostPressure = states
-  //   .map(state => pressureReleased(valves, 30)(state))
-  //   .sort(descending)[0]
-
-  // const mostPressure = depthFirstSearch(valves, "AA", [], 0)
+  console.log("Distance map between non zero valves (and start) calculated")
+  const paths = getPaths(distances)("AA", 30, ["AA"])
+  console.log(paths.length + " possible paths between valves found")
 
   console.log("(P1) Answer: ")
 }
@@ -58,62 +43,19 @@ const parseValve = (map: ValveMap, line: string): ValveMap => {
   return { ...map, [name]: {flowRate, tunnels } }
 }
 
-const getMoves = (valves: ValveMap) =>
-  (state: State, turn: number): State[] => {
-    const currentValve = valves[state.position]
-    const tunnelMoves = currentValve.tunnels.map(tunnel => 
-      ({ ...state, position: tunnel }))
-    
-    return state.openValves[state.position] || currentValve.flowRate === 0
-      ? tunnelMoves
-      : [
-          ...tunnelMoves,
-          {
-            ...state,
-            openValves: { ...state.openValves, [state.position]: turn } }
-        ]
-  }
-
-const stateCharacteristic = ({ position, openValves }: State) =>
-  `${position} - [${Object.keys(openValves).sort().join(",")}]`
-
-const pressureReleased = (valves: ValveMap, minute: number) => 
-  ({ openValves }: State): number =>
-    Object.keys(openValves).map(name => {
-      const minutesOpen = minute - openValves[name]
-      return valves[name].flowRate * minutesOpen
-    }).reduce(sum, 0)
-
-
-const depthFirstSearch = (
-  valves: ValveMap,
-  position: string,
-  open: string[],
-  minute: number
-): number => {
-  if (minute > 30) { return 0 }
-
-  const pressureReleased = open
-    .map(name => valves[name].flowRate)
-    .reduce(sum, 0)
-  
-  const openValve = open.includes(position)
-    ? -1
-    : depthFirstSearch(valves, position, [...open, position], minute + 1)
-  
-  const takeTunnels = valves[position].tunnels.map(name =>
-    depthFirstSearch(valves, name, open, minute + 1))
-
-  return pressureReleased + [openValve, ...takeTunnels].reduce(max)
-}
-
 const getPaths = (distances: {[k: string]: DistanceMap}) =>
   (from: string, minutesLeft: number, visited: string[]): string[][] => {
-    if (minutesLeft === 0) { return [[from]] }
-    return Object.keys(distances[from])
+    if (minutesLeft <= 0) { return [[from]] }
+    
+    const options = Object.keys(distances[from])
       .filter(next => !visited.includes(next))
+      .filter(next => distances[from][next] <= minutesLeft)
+    
+    if (options.length === 0) { return [[from]] }
+
+    return options
       .flatMap(next =>
-        getPaths(distances)(next, minutesLeft - 2, [...visited, from])
+        getPaths(distances)(next, minutesLeft - distances[from][next] - 1, [...visited, next])
           .map(path => [from, ...path]))
   }
 
@@ -135,7 +77,9 @@ const movementCosts = (valves: ValveMap) => (from: string): DistanceMap=> {
     updates = newUpdates
   }
 
-  return costs
+  return Object.fromEntries(Object.entries(costs)
+    .filter(([key]) => valves[key].flowRate > 0)
+    .filter(([key]) => key !== from))
 }
 
 partOne()
