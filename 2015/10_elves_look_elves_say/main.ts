09f421b5806a6fbe432ad365f3b2ef06e31fb26c
David Rendell <davidwrendell@gmail.com>
2022-12-21 18:25:51 +0000

    2015 Day 10 pt 2

--- 2015/10_elves_look_elves_say/main.ts
+++ 2015/10_elves_look_elves_say/main.ts
@@ -1,16 +1,44 @@
-const input = [..."1113222113"]
+type Repeat = [number, string]
 
-const getRepeats = (repeats: [number, string][], next: string): [number, string][] =>
-  repeats.at(-1) && repeats.at(-1)[1] === next
-    ? [...repeats.slice(0, -1), [repeats.at(-1)[0] + 1, next]]
-    : [...repeats, [1, next]]
+const memos: Map<string, Repeat[]> = new Map()
 
-let output = input
+const calculatePairs = (input: string): Repeat[] => {
+  const save = (output: Repeat[]) => {
+    memos.set(input, output)
+    return output
+  }
+  if (memos.has(input)) { return memos.get(input) }
+  if (input.length === 0) { return save([]) }
+  if (input.length === 1) { return save([[1, input[0]]]) }
+
+  const split = Math.floor(input.length / 2)
+  const left = input.slice(0, split)
+  const right = input.slice(split)
+  const leftPairs = calculatePairs(left)
+  const rightPairs = calculatePairs(right)
+
+  const leftEnd = leftPairs.at(-1)
+  const rightEnd = rightPairs.at(0)
+  
+  if (leftEnd[1] === rightEnd[1]) {
+    return save([
+      ...leftPairs.slice(0, -1),
+      [leftEnd[0] + rightEnd[0], leftEnd[1]],
+      ...rightPairs.slice(1),
+    ])
+  }
+  return save([...leftPairs, ...rightPairs])
+}
+
+let output = "1113222113"
 for (let i = 0; i < 40; i++) {
-  console.log(i)
-  output = [...output.reduce(getRepeats, []).flat().join("")]
+  output = calculatePairs(output).flat().join("")
 }
 
 console.log("(P1): " + output.length)
 
-console.log("(P2): " + 0)
+for (let i = 40; i < 50; i++) {
+  output = calculatePairs(output).flat().join("")
+}
+
+console.log("(P2): " + output.length)
