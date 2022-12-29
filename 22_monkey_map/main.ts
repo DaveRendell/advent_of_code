72d934d0a428a0d1186a2b29c56c29534d98bf74
David Rendell <davidwrendell@gmail.com>
2022-12-29 15:13:04 +0000

    Tidy up Day 22

--- 22_monkey_map/main.ts
+++ 22_monkey_map/main.ts
@@ -51,65 +51,32 @@ const faces: Face[] = net.map((row, rowId) =>
   .flat()
   .filter(({ grid }) => grid[0][0] !== " ")
 
-const wrapAroundEdges: Edges[] =
-  faces.map(({ netPosition}) => {
-    const getNextFaceInDirection = (direction: Vector2): number => {
-      let newFace = netPosition
-      do { 
-        newFace = newFace.add(direction)
-        newFace.y = positiveMod(newFace.y, net.length)
-        newFace.x = positiveMod(newFace.x, net[newFace.y].length)
-      } while (net[newFace.y][newFace.x][0][0] === " ")
-      return faces.findIndex(({netPosition}) => newFace.equals(netPosition))
-    }
-    return [
-      { faceId: getNextFaceInDirection(Vector2.RIGHT), rotation: 0 },
-      { faceId: getNextFaceInDirection(Vector2.DOWN), rotation: 0 },
-      { faceId: getNextFaceInDirection(Vector2.LEFT), rotation: 0 },
-      { faceId: getNextFaceInDirection(Vector2.UP), rotation: 0 },
-    ]
-  })
+const positionOnEdge = (directionId: number, fromLeft: number): Vector2 => {
+  if (directionId == 0) { return new Vector2(0, fromLeft) } // R
+  if (directionId == 1) { return new Vector2(faceLength - fromLeft - 1, 0) } // D
+  if (directionId == 2) { return new Vector2(faceLength - 1, faceLength - fromLeft - 1) } // L
+  if (directionId == 3) { return new Vector2(fromLeft, faceLength - 1) } // U
+}
 
 const step = (edges: Edges[]) => ({ face, faceCoordinates, directionId }: Position): Position => {
-  const inDirection = (dir: number, fromLeft: number): Vector2 => {
-    if (dir == 0) { return new Vector2(0, fromLeft) } // R
-    if (dir == 1) { return new Vector2(faceLength - fromLeft - 1, 0) } // D
-    if (dir == 2) { return new Vector2(faceLength - 1, faceLength - fromLeft - 1) } // L
-    if (dir == 3) { return new Vector2(fromLeft, faceLength - 1) } // U
-  }
   const newCoordinates = faceCoordinates.add(directions[directionId])
-  if (newCoordinates.x >= faceLength) {
-    const newDirection = positiveMod(directionId + edges[face][0].rotation, 4)
+  const { x, y } = newCoordinates
+
+  const crossEdge = (fromLeft: number): Position => {
+    const edge = edges[face][directionId]
+    const newDirection = positiveMod(directionId + edge.rotation, 4)
     return {
-      face: edges[face][0].faceId,
+      face: edge.faceId,
       directionId: newDirection,
-      faceCoordinates: inDirection(newDirection, newCoordinates.y)
+      faceCoordinates: positionOnEdge(newDirection, fromLeft)
     }
   }
-  if (newCoordinates.x < 0) {
-    const newDirection = positiveMod(directionId + edges[face][2].rotation, 4)
-    return {
-      face: edges[face][2].faceId,
-      directionId: newDirection,
-      faceCoordinates: inDirection(newDirection, faceLength - newCoordinates.y - 1)
-    }
-  }
-  if (newCoordinates.y >= faceLength) {
-    const newDirection = positiveMod(directionId + edges[face][1].rotation, 4)
-    return {
-      face: edges[face][1].faceId,
-      directionId: newDirection,
-      faceCoordinates: inDirection(newDirection, faceLength - newCoordinates.x - 1)
-    }
-  }
-  if (newCoordinates.y < 0) {
-    const newDirection = positiveMod(directionId + edges[face][3].rotation, 4)
-    return {
-      face: edges[face][3].faceId,
-      directionId: newDirection,
-      faceCoordinates: inDirection(newDirection, newCoordinates.x)
-    }
-  }
+
+  if (x >= faceLength) { return crossEdge(y) }
+  if (x < 0) { return crossEdge(faceLength - y - 1) }
+  if (y >= faceLength) { return crossEdge(faceLength - x - 1) }
+  if (y < 0) { return crossEdge(x) }
+
   return {
     faceCoordinates: newCoordinates,
     face, directionId
@@ -140,6 +107,25 @@ const traverse = (transitions: Edges[]): Position => {
   }
   return position
 }
+
+const wrapAroundEdges: Edges[] =
+  faces.map(({ netPosition}) => {
+    const getNextFaceInDirection = (direction: Vector2): number => {
+      let newFace = netPosition
+      do { 
+        newFace = newFace.add(direction)
+        newFace.y = positiveMod(newFace.y, net.length)
+        newFace.x = positiveMod(newFace.x, net[newFace.y].length)
+      } while (net[newFace.y][newFace.x][0][0] === " ")
+      return faces.findIndex(({netPosition}) => newFace.equals(netPosition))
+    }
+    return [
+      { faceId: getNextFaceInDirection(Vector2.RIGHT), rotation: 0 },
+      { faceId: getNextFaceInDirection(Vector2.DOWN), rotation: 0 },
+      { faceId: getNextFaceInDirection(Vector2.LEFT), rotation: 0 },
+      { faceId: getNextFaceInDirection(Vector2.UP), rotation: 0 },
+    ]
+  })
 
 const getCubeEdge = (face: Face, directionId: number): Edge => {
   const candidate = face.netPosition.add(directions[directionId])
