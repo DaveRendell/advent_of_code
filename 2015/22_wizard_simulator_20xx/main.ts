8bcb33abf8b712e5417f9a8b92660c98f506447e
David Rendell <davidwrendell@gmail.com>
2022-12-30 12:21:35 +0000

    2015 day 22

--- 2015/22_wizard_simulator_20xx/main.ts
+++ 2015/22_wizard_simulator_20xx/main.ts
@@ -24,7 +24,6 @@ interface State {
   log: string[],
 }
 const SPELLS: Spell[] = [
-  { mana: 0 },
   { mana: 53, damage: 4 },
   { mana: 73, damage: 2, heal: 2 },
   { mana: 113, effect: { name: "shield", turns: 6, armour: 7 } },
@@ -32,35 +31,79 @@ const SPELLS: Spell[] = [
   { mana: 229, effect: { name: "rechge", turns: 5, mana: 101 } },
 ]
 
-const start: State = {
-  turn: "player",
-  playerHP: 50,
-  bossHP: 51,
-  mana: 500,
-  effects: [],
-  manaSpent: 0,
-  log: []
-}
-
-let inProgress: Queue<State> = new Queue()
-let leastMana = Infinity
-
-inProgress.add(start)
-
-bfs:
-while (inProgress.hasNext()) {
-  // Apply effects
-  // Decrement counters
-  // Check for boss death
-  
-  let state = inProgress.receive()
-  if (state.turn === "player") {
-    // iterate over possible spells
-  } else {
-    // do damage, check for player death
+const simulate = (chipDamage: number): number => {
+  const start: State = {
+    turn: "player",
+    playerHP: 50,
+    bossHP: 51,
+    mana: 500,
+    effects: [],
+    manaSpent: 0,
+    log: []
   }
+  
+  let inProgress: Queue<State> = new Queue()
+  let leastMana = Infinity
+  
+  inProgress.add(start)
+  
+  bfs:
+  while (inProgress.hasNext()) {
+    let state = inProgress.receive()
+    
+    if (state.manaSpent >= leastMana) { continue bfs }
+    const playerStartHp = state.turn === "player" 
+      ? state.playerHP - chipDamage
+      : state.playerHP
+    if (playerStartHp <= 0) { continue bfs }   
+  
+    const armour =
+      state.effects.map(({armour}) => armour ?? 0).reduce(sum, 0)
+    const bossStartHp = state.bossHP
+      - state.effects.map(({damage}) => damage ?? 0).reduce(sum, 0)
+    const mana = state.mana
+      + state.effects.map(({mana}) => mana ?? 0).reduce(sum, 0)
+    const effects = state.effects
+      .map(effect => ({ ...effect, turns: effect.turns - 1 }))
+      .filter(effect => effect.turns > 0)
+  
+    if (bossStartHp <= 0) {
+      leastMana = Math.min(leastMana, state.manaSpent)
+      continue bfs
+    }
+  
+    if (state.turn === "player") {
+      SPELLS
+        .filter(spell => spell.mana <= mana)
+        .filter(spell => spell.effect === undefined
+          || !effects.some(({name}) => name === spell.effect.name))
+        .forEach(spell => inProgress.add({
+          turn: "boss",
+          playerHP: playerStartHp + (spell.heal ?? 0),
+          bossHP: bossStartHp - (spell.damage ?? 0),
+          mana: mana - spell.mana,
+          effects: spell.effect ? [...effects, spell.effect] : effects,
+          manaSpent: state.manaSpent + spell.mana,
+          log: state.log
+        }))
+    } else {
+      const playerHP = playerStartHp - Math.max(1, 9 - armour)
+      if (playerHP <= 0) {
+        continue bfs
+      }
+      inProgress.add({
+        turn: "player",
+        playerHP,
+        bossHP: bossStartHp,
+        mana,
+        effects,
+        manaSpent: state.manaSpent,
+        log: state.log
+      })
+    }
+  }
+  return leastMana
 }
 
-console.log("(P1): " + 0)
-
-console.log("(P2): " + 0)
+console.log("(P1): " + simulate(0))
+console.log("(P2): " + simulate(1))
