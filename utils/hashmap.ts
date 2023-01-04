4da50b77a8bd053e8da91e18cf0b33c191649dc7
David Rendell <David.Rendell@Softwire.com>
2023-01-04 10:29:28 +0000

    First draft hash set and hash map helpers

---
+++ utils/hashmap.ts
@@ -0,0 +1,47 @@
+export default class HashMap<Key, Value> {
+  hash: (Key) => string
+  size: number
+  
+  private data: Map<string, [Key, Value]>
+
+  constructor(hash: (Key) => string, data: [Key, Value][]) {
+    this.hash = hash
+    this.data = new Map(data.map(([key, value]) => [hash(key), [key, value]]))
+    this.size = this.data.size
+  }
+
+  set(key: Key, value: Value) {
+    this.data.set(this.hash(key), [key, value])
+    this.size = this.data.size
+  }
+
+  get(key: Key): Value {
+    return this.data.get(this.hash(key))[1]
+  }
+
+  has(key: Key): boolean {
+    return this.data.has(this.hash(key))
+  }
+
+  remove(key: Key) {
+    this.data.delete(this.hash(key))
+    this.size = this.data.size
+  }
+
+  clear() {
+    this.data.clear()
+    this.size = this.data.size
+  }
+
+  entries(): [Key, Value][] {
+    return [...this.data.values()]
+  }
+
+  keys(): Key[] {
+    return this.entries().map(([k]) => k)
+  }
+
+  values(): Value[] {
+    return this.entries().map(([_, v]) => v)
+  }
+}
---
+++ utils/hashset.ts
@@ -0,0 +1,35 @@
+export default class HashSet<Item> {
+  hash: (Item) => string
+  size: number
+
+  private data: Map<string, Item>
+
+  constructor(hash: (Item) => string, data: Item[] = []) {
+    this.hash = hash
+    this.data = new Map(data.map(item => [hash(item), item]))
+    this.size = this.data.size
+  }
+
+  add(item: Item): void {
+    this.data.set(this.hash(item), item)
+    this.size = this.data.size
+  }
+
+  remove(item: Item): void {
+    this.data.delete(this.hash(item))
+    this.size = this.data.size
+  }
+
+  has(item: Item): boolean {
+    return this.data.has(this.hash(item))
+  }
+
+  clear() {
+    this.data.clear()
+    this.size = this.data.size
+  }
+
+  entries(): Item[] {
+    return [...this.data.values()]
+  }
+}
