3430be203758538a3797d427982ec67e13e0e66d
David Rendell <davidwrendell@gmail.com>
2023-11-02 18:20:16 +0000

    2016 day 19

--- 2016/19_an_elephant_named_joseph/main.ts
+++ 2016/19_an_elephant_named_joseph/main.ts
@@ -1,8 +1,54 @@
-import readLines from "../../utils/readLines"
-import inputFile from "../../utils/inputFile"
+import Cycle from "../../utils/cycle"
+import { range } from "../../utils/numbers"
 
-const input = readLines(__dirname, inputFile())
+interface Elf { id: number, presents: number }
 
-console.log("(P1): " + 0)
+function calculateLuckyElfAdj(numberOfElves: number): number {
+  let elfCircle: Cycle<Elf> = Cycle.fromValues(range(0, numberOfElves)
+    .map(i => ({ id: i + 1, presents: 1 })))
 
-console.log("(P2): " + 0)
+  while (elfCircle.next != elfCircle) {
+    if (elfCircle.value.presents == 0) {
+      elfCircle = elfCircle.next
+      elfCircle.removePrevious()
+    } else {
+      elfCircle.value = {
+        ...elfCircle.value,
+        presents: elfCircle.value.presents + elfCircle.next.value.presents
+      }
+      elfCircle.next.value = {
+        ...elfCircle.next.value,
+        presents: 0
+      }
+      elfCircle = elfCircle.next
+    }
+  }
+  return elfCircle.value.id
+}
+
+function calculateLuckyElfOpposite(numberOfElves: number): number {
+  let elves = range(1, numberOfElves + 1)
+  let elfCount = numberOfElves
+  let currentElfIndex = 0
+
+  while (elfCount > 1) {
+    let numberToSearch = Math.floor(elfCount / 2)
+    let searchIndex = currentElfIndex
+    while (numberToSearch > 0) {
+      if (elves[++searchIndex]) { numberToSearch-- }
+    }
+    elves[searchIndex] = 0
+    elfCount--
+    while(elves[++currentElfIndex]) {}
+  }
+  return elves.find(elf => elf)
+}
+
+const testCycle = Cycle.fromValues(["a", "b", "c", "d", "e"])
+
+testCycle.removeAtPositionAhead(2)
+console.log(testCycle.getNext(8))
+
+console.log("(P1): " + calculateLuckyElfAdj(5))
+
+console.log("(P2): " + calculateLuckyElfOpposite(5))
--- utils/cycle.ts
+++ utils/cycle.ts
@@ -19,6 +19,19 @@ export default class Cycle<T> {
     return values
   }
 
+  removePrevious(): void {
+    const oldPrevious = this.previous
+    this.previous = oldPrevious.previous
+    oldPrevious.previous.next = this
+  }
+
+  removeAtPositionAhead(position: number): void {
+    let toRemove: Cycle<T> = this
+    for (let i = 0; i < position; i++) { toRemove = toRemove.next }
+    toRemove.next.previous = toRemove.previous
+    toRemove.previous.next = toRemove.next
+  }
+
   static fromValues<S>(values: S[]): Cycle<S> {
     const links = values.map(v => new Cycle(v))
     links.forEach((link, index) => {
