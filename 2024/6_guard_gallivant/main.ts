8271da6883ac57aa4771eb2719b1cdf6ef32dde7
David Rendell <David.Rendell@Softwire.com>
2024-12-06 17:27:59 +0000

    2024 day 6 part 2 fix

--- 2024/6_guard_gallivant/main.ts
+++ 2024/6_guard_gallivant/main.ts
@@ -1,3 +1,4 @@
+import HashSet from "../../utils/hashset"
 import inputFile from "../../utils/inputFile"
 import readCharArray from "../../utils/readCharArray"
 import Vector2, { VectorMap, VectorSet } from "../../utils/vector2"
@@ -8,50 +9,51 @@ const grid = new VectorMap<string>(input.flatMap((row, y) =>
   row.map((value, x) => [new Vector2(x, y), value] as [Vector2, string])))
 
 const initialRow = input.findIndex(row => row.includes("^"))
-let guardPosition = new Vector2(
+const initialPosition = new Vector2(
   input[initialRow].findIndex(x => x === "^"), initialRow)
+let guardPosition = initialPosition
 let guardDirection = Vector2.UP
 
 const visitedLocations = new VectorSet()
-const loopLocations = new VectorSet()
 
 while (guardPosition.inGrid(input[0].length, input.length)) {
   visitedLocations.add(guardPosition)
-  console.log(visitedLocations.size, "loops found:", loopLocations.size)
   if (grid.get(guardPosition.add(guardDirection)) === "#") {
     guardDirection = guardDirection.rotateRight()
   } else {
-    // Try placing obstacle ahead
-    const obstructedGrid = new VectorMap(grid.entries())
-    obstructedGrid.set(guardPosition.add(guardDirection), "#")
-    let projectedGuardPosition = guardPosition.add(Vector2.ORIGIN)
-    let projectedGuardDirection = guardDirection.add(Vector2.ORIGIN)
-
-    let loopCheck = new Set<string>()
-
-    projectionLoop:
-    while (projectedGuardPosition.inGrid(input[0].length, input.length)) {
-      if (loopCheck.has(projectedGuardPosition.toString() + projectedGuardDirection.toString())) {
-        loopLocations.add(guardPosition.add(guardDirection))
-        break projectionLoop
-      }
-
-      loopCheck.add(projectedGuardPosition.toString() + projectedGuardDirection.toString())
-
-      if (obstructedGrid.get(projectedGuardPosition.add(projectedGuardDirection)) === "#") {
-        projectedGuardDirection = projectedGuardDirection.rotateRight()
-      } else {
-        projectedGuardPosition = projectedGuardPosition.add(projectedGuardDirection)
-      }
-    }
-
-
     guardPosition = guardPosition.add(guardDirection)
   }
 }
 
-console.log("(P1): ", visitedLocations.size) 
+console.log("(P1): ", visitedLocations.size)
+
+const loopLocations = new VectorSet(visitedLocations.entries()
+  .filter((location, i, a) => {
+    console.log(`${i} / ${a.length}`)
+    const obstructedGrid = new VectorMap(grid.entries())
+    obstructedGrid.set(location, "#")
+
+    let guardPosition = initialPosition
+    let guardDirection = Vector2.UP
+
+    const loopCheck = new Set<string>()
+    let isLoop = false
+
+    testLoop:
+    while (guardPosition.inGrid(input[0].length, input.length)) {
+      if (loopCheck.has(guardPosition.toString() + guardDirection.toString())) {
+        isLoop = true
+        break testLoop
+      }
+      loopCheck.add(guardPosition.toString() + guardDirection.toString())
+      if (obstructedGrid.get(guardPosition.add(guardDirection)) === "#") {
+        guardDirection = guardDirection.rotateRight()
+      } else {
+        guardPosition = guardPosition.add(guardDirection)
+      }
+    }
+
+    return isLoop
+  }))
 
 console.log("(P2): ", loopLocations.size) // 1354 too high
-
-console.log(loopLocations.entries())
